4d611a074168b7db43e3258ef12acdc9235db4c8
Edder Luan <eluann100@gmail.com>
2025-04-14 21:41:23 -0300

    First commit

---
+++ melembraai/src/main/java/com/edderluanps/melembraai/MelembraaiApplication.java
@@ -0,0 +1,13 @@
+package com.edderluanps.melembraai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelembraaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelembraaiApplication.class, args);
+	}
+
+}
